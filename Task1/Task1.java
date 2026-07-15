d52d0ba74b4a771e2a4300528073ded93dd2204a
DaniDan05 <icctcatay05@gmail.com>
2026-07-15 08:50:22 +0000

    fixed: Add missing characters and modify the code.

---
+++ Task1/Task1.java
@@ -0,0 +1,12 @@
+
+public class Task1 {
+    /**
+     * The main method is the entry point of the program.
+     * Prints "Kamusta Mundo!" to the console.
+     * @param args
+     * @author DaniDan05
+     */
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
