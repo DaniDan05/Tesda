4e5e0836d42ec242e4bba8c6cd7d29e529b991f1
DaniDan <icctcatay05@gmail.com>
2026-07-23 18:56:52 +0800

    fixed: Replaced String Values

---
+++ Task3/Task3.java
@@ -0,0 +1,21 @@
+package Tesda.Task3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        // String a = new String("Wow");
+        String a = "Wow";
+        String b = "Wow";
+        // String c = a;
+        String c = new String("Kahit Ano nyahahaha.");
+        // String d = c;
+        String d = new String("Wow!");
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
