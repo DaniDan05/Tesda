f7e5c3f6fd090dbabcd928741a7ec308f99dad2d
DaniDan <icctcatay05@gmail.com>
2026-07-23 17:14:50 +0800

    add: Task2 complete

---
+++ Task2/Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte b = 3;
+        char c = '1';
+        short s = 10;
+        int i = 0;
+        float f = 2.0f;
+        boolean bool = true;
+        String output = "H" + b + c + s + " w" + i + "rld " + f + " " + bool; 
+        System.out.println(output);
+    }
+}
